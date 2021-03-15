3515f6fe74d807fc638cd7dbcfb4476152906323
linuxmonk <monklinux5@gmail.com>
2021-03-15 13:37:50 +1100

    Add a v2 of selection sort after removing unwanted variables

--- sorting/selection/selection.go
+++ sorting/selection/selection.go
@@ -1,6 +1,6 @@
 package selection
 
-func SelectionSort(list []int) {
+func SelectionSortV1(list []int) {
 
 	if list == nil || len(list) <= 1 {
 		return
@@ -22,5 +22,29 @@ func SelectionSort(list []int) {
 			list[unsortedIndex], list[lowestIndex] = list[lowestIndex], list[unsortedIndex]
 		}
 		unsortedIndex++
+	}
+}
+
+// Remove unwanted variables from V1 and more compact code.
+// !!This seems slower due to extra operations of indexing into array etc.!!
+func SelectionSortV2(list []int) {
+
+	if list == nil || len(list) <= 1 {
+		return
+	}
+
+	n := len(list)
+	for i := 0; i < n; i++ {
+		lowestIndex := i
+		found := false
+		for j := i; j < n; j++ {
+			if list[j] < list[lowestIndex] {
+				lowestIndex = j
+				found = true
+			}
+		}
+		if found {
+			list[i], list[lowestIndex] = list[lowestIndex], list[i]
+		}
 	}
 }
--- sorting/selection/selection_test.go
+++ sorting/selection/selection_test.go
@@ -1,60 +1,77 @@
 package selection
 
 import (
+	"os"
 	"reflect"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSelectionSort(t *testing.T) {
+func testSelectionSort(t *testing.T, alg func([]int)) {
 
 	tests := []struct {
 		name     string
+		testfn   func([]int)
 		input    []int
 		expected []int
 	}{
 		{
 			name:     "UNSORTED_LIST",
+			testfn:   alg,
 			input:    []int{2, 4, 1, 3, 7},
 			expected: []int{1, 2, 3, 4, 7},
 		},
 		{
 			name:     "LIST_IN_DESCENDING_ORDER",
+			testfn:   alg,
 			input:    []int{5, 4, 3, 2, 1},
 			expected: []int{1, 2, 3, 4, 5},
 		},
 		{
 			name:     "LIST_ALREADY_IN_ORDER",
+			testfn:   alg,
 			input:    []int{1, 2, 3, 4, 5},
 			expected: []int{1, 2, 3, 4, 5},
 		},
 		{
 			name:     "TWO_ELEMENT_LIST_UNORDERED",
+			testfn:   alg,
 			input:    []int{2, 1},
 			expected: []int{1, 2},
 		},
 		{
 			name:     "SINGLE_ELEMENT_LIST",
+			testfn:   alg,
 			input:    []int{1},
 			expected: []int{1},
 		},
 		{
 			name:     "NIL_LIST",
+			testfn:   alg,
 			input:    nil,
 			expected: nil,
 		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			SelectionSort(test.input)
+			alg(test.input)
 			equal := reflect.DeepEqual(test.input, test.expected)
 			assert.Equal(t, equal, true)
 		})
 	}
 }
 
-func BenchmarkSelectionSort(b *testing.B) {
+func TestSelectionSort(t *testing.T) {
+	testSelectionSort(t, SelectionSortV1)
+}
+
+func TestSelectionSortV2(t *testing.T) {
+	testSelectionSort(t, SelectionSortV2)
+}
+
+func benchSelectionSort(b *testing.B, alg func([]int)) {
 	b.StopTimer()
 	unsorted := make([]int, 1<<10)
 	for i := range unsorted {
@@ -64,8 +81,17 @@ func BenchmarkSelectionSort(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		copy(data, unsorted)
 		b.StartTimer()
-		SelectionSort(data)
+		alg(data)
 		b.StopTimer()
 	}
 	b.StopTimer()
+}
+
+func BenchmarkSelectionSort(b *testing.B) {
+	v := os.Getenv("V")
+	if v == "" || strings.TrimSpace(v) == "1" {
+		benchSelectionSort(b, SelectionSortV1)
+	} else {
+		benchSelectionSort(b, SelectionSortV2)
+	}
 }
