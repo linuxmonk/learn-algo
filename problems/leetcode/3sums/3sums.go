fe9f325eac25fa8172c7747be27932e9da13bbe0
linuxmonk <monklinux5@gmail.com>
2021-04-13 15:06:54 +1000

    3Sums pointer method
    * BUG: Returns duplicate values

--- problems/leetcode/3sums/3sums.go
+++ problems/leetcode/3sums/3sums.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	input := []int{-1, 0, 1, 2, -1, -4}
@@ -16,26 +19,47 @@ func threeSum(nums []int) [][]int {
 	if len(nums) < 3 {
 		return out
 	}
-	processedIndex := make(map[int]int)
+	sort.Ints(nums)
 	n := len(nums)
 	for i := 0; i < n; i++ {
-		for j := i + 1; j < n; j++ {
-			for k := j + 1; k < n; k++ {
-				if i != j && i != k && j != k {
-					if nums[i]+nums[j]+nums[k] == 0 {
-						if processedIndex[i] && processedIndex[j] && processedIndex[k] {
-							continue
-						}
-						sum := make([]int, 0)
-						sum = append(sum, nums[i], nums[j], nums[k])
-						out = append(out, sum)
-						processedIndex[i] = true
-						processedIndex[j] = true
-						processedIndex[k] = true
-					}
-				}
-			}
-		}
+		threeSumII(nums, i, &out)
 	}
 	return out
+}
+
+func threeSumII(nums []int, i int, out *[][]int) {
+	lo := i + 1
+	hi := len(nums) - 1
+	for lo < hi {
+		threeSum := nums[lo] + nums[i] + nums[hi]
+		if threeSum < 0 {
+			lo++
+			for {
+				if lo < hi && nums[lo] == nums[lo+1] {
+					lo++
+					continue
+				}
+				break
+			}
+			continue
+		}
+		if threeSum > 0 {
+			hi--
+			for {
+				if hi > lo && nums[hi] == nums[hi-1] {
+					hi--
+					continue
+				}
+				break
+			}
+		}
+		if threeSum == 0 {
+			*out = append(*out, []int{nums[lo], nums[i], nums[hi]})
+			lo++
+			for i == lo {
+				lo++
+			}
+			hi--
+		}
+	}
 }
