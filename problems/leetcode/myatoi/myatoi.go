d4aed7c77be9634470a802039bab712baeb09f24
linuxmonk <monklinux5@gmail.com>
2021-04-16 15:48:04 +1000

    fix all cases except the corner cases which c++ atoi parses out

--- problems/leetcode/myatoi/myatoi.go
+++ problems/leetcode/myatoi/myatoi.go
@@ -3,37 +3,79 @@ package main
 import (
 	"fmt"
 	"math"
+	"strings"
 )
+
+func isDigit(c byte) bool {
+	if c >= '0' && c <= '9' {
+		return true
+	}
+	return false
+}
+
+func isSign(c byte) bool {
+	if c == '-' || c == '+' {
+		return true
+	}
+	return false
+}
+
+func isNum(word string) bool {
+
+	var dot bool
+
+	if len(word) == 0 {
+		return false
+	}
+	i := 0
+	if isSign(word[0]) {
+		i = 1
+	}
+	// could be a number without a +
+	for ; i < len(word); i++ {
+		if isDigit(word[i]) {
+			continue
+		}
+		if word[i] == '.' && !dot {
+			dot = true
+			continue
+		}
+		return false
+	}
+	return true
+}
 
 func myAtoi(s string) int {
 
-	n := len(s)
-	num := 0
-	foundNums := false
-	sign := 1
-	i := 0
-	done := false
-	// eat white spaces
-	for s[i] == ' ' {
-		i++
-	}
-	for ; i < n; i++ {
-		if done {
-			break
+	var num, sign int
+
+	s = strings.TrimSpace(s)
+	sign = 1
+	slist := strings.Split(s, " ")
+
+	for _, word := range slist {
+		if word == "" {
+			continue
 		}
-		switch {
-		case s[i] == '-':
-			if !foundNums {
+		if !isNum(word) {
+			return 0
+		}
+		i := 0
+		if isSign(word[0]) {
+			if word[0] == '-' {
 				sign = -1
 			}
-		case s[i] >= '0' && s[i] <= '9':
-			foundNums = true
-			num = (10 * num) + int(s[i]-'0')
-		default:
-			done = true
-			break
+			i = 1
 		}
+		for ; i < len(word); i++ {
+			if word[i] == '.' {
+				break
+			}
+			num = (10 * num) + int(word[i]-'0')
+		}
+		break
 	}
+
 	ret := num * sign
 	if ret <= math.MinInt32 {
 		return math.MinInt32
@@ -41,7 +83,7 @@ func myAtoi(s string) int {
 	if ret >= math.MaxInt32 {
 		return math.MaxInt32
 	}
-	return num * sign
+	return ret
 }
 
 func main() {
@@ -60,5 +102,13 @@ func main() {
 	input = " words with two numbers -9345 and 987"
 	fmt.Printf("input = %v, output = %v\n", input, myAtoi(input))
 	input = "-91283472332"
+	fmt.Printf("input = %v, output = %v\n", input, myAtoi(input))
+	input = "+-12"
+	fmt.Printf("input = %v, output = %v\n", input, myAtoi(input))
+	input = "-+12"
+	fmt.Printf("input = %v, output = %v\n", input, myAtoi(input))
+	input = "+12"
+	fmt.Printf("input = %v, output = %v\n", input, myAtoi(input))
+	input = "3.145632"
 	fmt.Printf("input = %v, output = %v\n", input, myAtoi(input))
 }
